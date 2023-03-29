fbae1f60851fd171506c4ec1e118d7b5def02d81
manish-4007 <manish.rai709130@gmail.com>
2023-03-29 12:10:36 +0530

    wepage style changes

--- src/pipeline/predict_pipeline.py
+++ src/pipeline/predict_pipeline.py
@@ -9,8 +9,8 @@ class PredictPipeline:
 
     def predict(self,features):
         try:
-            model_path = 'artifacts\model.pkl'
-            preprocessor_path = 'artifacts\preprocessor.pkl'
+            model_path = 'artifacts/model.pkl'
+            preprocessor_path = 'artifacts/preprocessor.pkl'
             model = load_object(model_path)
             preprocessor = load_object(preprocessor_path)
             
